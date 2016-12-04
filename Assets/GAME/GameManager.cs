1d65c260f763cebd63cee3002b40cd60f66e12cc
Michael Johnson <michaelfjohnson@gmail.com>
2016-12-04 14:54:23 -0800

    end of lesson

---
+++ Assets/GAME/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+	public bool recording = true;
+
+	private bool isPaused = false;
+	private float fixedDeltaTime;
+
+	void Start () {
+		PlayerPrefsManager.UnlockLevel (2);
+		fixedDeltaTime = Time.fixedDeltaTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButton("Fire1")) {
+			recording = false;
+		} else {
+			recording = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.P) && isPaused) {
+			isPaused = false;
+			ResumeGame();
+		} else if (Input.GetKeyDown (KeyCode.P) && !isPaused) {
+			isPaused = true;
+			PauseGame();
+		}
+	}
+
+	void PauseGame() {
+		Time.timeScale = 0;
+		Time.fixedDeltaTime = 0;
+	}
+	
+	void ResumeGame() {
+		Time.timeScale = 1f;
+		Time.fixedDeltaTime = fixedDeltaTime;
+	}
+
+	void OnApplicationPause( bool pauseStatus ) {
+		isPaused = pauseStatus;
+	}
+
+}
